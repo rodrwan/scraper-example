7af6dbf6158558fd0026c114c611e930fa08e8fd
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2015-04-16 15:13:24 -0300

    upgrade yakuza, change plan declaration

--- bikes/terra-firma/terra-firma.agent.js
+++ bikes/terra-firma/terra-firma.agent.js
@@ -10,12 +10,10 @@ require('./get-shop-link/get-shop-link.task');
 require('./get-product-link/get-product-link.task');
 
 Yakuza.agent('Bikes', 'TerraFirma')
-  .setup(function (config) {
-    config.plan = [
-      'GetShopLink',
-      'GetProductLink'
-    ];
-  })
+  .plan([
+    'GetShopLink',
+    'GetProductLink'
+  ])
   .routine('FirstRun', [
     'GetShopLink',
     'GetProductLink'
